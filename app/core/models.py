e68872a9e437408ba854a9fcfb49999e4d2bb099
smankenb <sean.ankenbruck@gmail.com>
2022-06-13 23:21:28 -0400

    completed user model, test admin user creation

--- app/core/models.py
+++ app/core/models.py
@@ -16,13 +16,26 @@ class UserManager(BaseUserManager):
         """Create, save and return user"""
         # Validate username
         if not username:
-            raise ValueError('User must have unique name.')
+            raise ValueError('User must have valid username.')
         # Validate email
         if not email:
             raise ValueError('User must have valid email address.')
         # Create user
-        user = self.model(username=username, email=self.normalize_email(email), **extra_fields)
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            **extra_fields
+        )
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        """Create and return new superuser"""
+        user = self.create_user(username, email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
@@ -33,8 +46,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,8 +9,8 @@ class ModelTests(TestCase):
     """Test models"""
 
     def test_create_user_with_email(self):
-        """Test ability to create user with username and email"""
-        username='test1'
+        """Test ability to create user with username and email."""
+        username = 'test1'
         email = 'test@example.com'
         password = 'password@1234'
         user = get_user_model().objects.create_user(
@@ -23,24 +23,40 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_normalize_user_email(self):
-        """Test email normalization"""
+        """Test email normalization."""
         email_list = [
             ["user1", "user1@EXAMPLE.com", "user1@example.com"],
             ["user2", "User2@Example.com", "User2@example.com"],
             ["user3", "USER3@EXAMPLE.COM", "USER3@example.com"],
         ]
         for name, email, normalized in email_list:
-            user = get_user_model().objects.create_user(name, email, 'password123')
+            user = get_user_model().objects.create_user(
+                name,
+                email,
+                'password123'
+            )
             self.assertEqual(user.email, normalized)
 
     def test_empty_username_throws_error(self):
-        """Test that creating user without a username throws error"""
+        """Test that creating user without a username throws error."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', 'user1@example.com', 'password123')
+            get_user_model().objects.create_user(
+                '',
+                'user1@example.com',
+                'password123'
+            )
 
     def test_empty_email_throws_error(self):
-        """Test that creating user without an email throws error"""
+        """Test that creating user without an email throws error."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('user1', '', 'password123')
 
-
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'superuser1',
+            'superuser@example.com',
+            'password@123!'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
